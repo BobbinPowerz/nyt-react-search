a0f4aa85c6c50d59fd6b62dc27021a81068876b5
unknown <bobbin.powerz@gmail.com>
2017-12-19 08:45:20 -0800

    starter code

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Books from "./pages/Books";
+import Nav from "./components/Nav";
+import DeleteBtn from "./components/DeleteBtn";
+const App = () => 
+  <div>
+    <Nav />
+    <Books />
+  </div>;
+
+export default App;
